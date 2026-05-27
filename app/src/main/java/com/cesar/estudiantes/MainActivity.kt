80f120d034b6f4aec997c4af66afa2d626a6862d
César Yana <cmdoyana39@gmail.com>
2026-05-26 20:31:47 -0500

    Primera version de la logica de los editTexts y Botones (inicializacion)

--- app/src/main/java/com/cesar/estudiantes/MainActivity.kt
+++ app/src/main/java/com/cesar/estudiantes/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.cesar.estudiantes
 
+import android.widget.*
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.database.FirebaseDatabase
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editNombre: EditText
+    private lateinit var editCurso: EditText
+    private lateinit var editCarrera: EditText
+
+    private lateinit var btnGuardar: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.app)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        editNombre = findViewById(R.id.editNombre)
+        editCurso = findViewById(R.id.editCurso)
+        editCarrera = findViewById(R.id.editCarrera)
+
+        btnGuardar = findViewById(R.id.btnGuardar)
+
+
+
     }
+
 }
+
+
+
+
